8246a2228852fd57dd8c8b5d863f4a8956c52fed
bancho <riceomeletomuraisu@gmail.com>
2025-09-06 23:21:44 +0900

    supabase関連のコメント打ったよ

--- lib/api/travelPlans.ts
+++ lib/api/travelPlans.ts
@@ -2,35 +2,36 @@ import { supabase } from '../supabaseClient';
 import { TravelPlan } from '../../app/types/travel';
 //supabaseのプラン情報を取得、追加、更新するやつら
 
-
 // 一覧取得
 export async function getTravelPlans(): Promise<TravelPlan[]> {
+    //dataにデータベースからとってきたものをcreated_atの小さい順でもってくる
     const { data, error } = await supabase
         .from('travel_plans')
         .select('*')
         .order('created_at', { ascending: false });
 
+    //エラーあったらエラーだすよ
     if (error) throw error;
-    return data as TravelPlan[];
+    return data as TravelPlan[]; //型を明記
 }
 
 // 新規追加
-export async function addTravelPlan(plan: Omit<TravelPlan, 'id' | 'createdAt' | 'updatedAt'>) {
+export async function addTravelPlan(plan: Omit<TravelPlan, 'id' | 'createdAt' | 'updatedAt'>) { //Omitは'id''createdAt''updateAt'という要素を省いた状態のクラスを返すもの
     const { data, error } = await supabase
         .from('travel_plans')
-        .insert([{ ...plan, created_at: new Date(), updated_at: new Date() }])
-        .select();
+        .insert([{ ...plan, created_at: new Date(), updated_at: new Date() }]) //new Date()は今の日付で入力される、要素一個の配列を挿入
+        .select(); //データ持ってくる
 
     if (error) throw error;
     return data[0];
 }
 
 // 更新
-export async function updateTravelPlan(id: string, updates: Partial<TravelPlan>) {
+export async function updateTravelPlan(id: string, updates: Partial<TravelPlan>) { //Partial型はTravelPlanの一部だけでも引数にしていいよっていう型
     const { data, error } = await supabase
         .from('travel_plans')
-        .update({ ...updates, updated_at: new Date() })
-        .eq('id', id)
+        .update({ ...updates, updated_at: new Date() }) //渡された引数のところだけ追加しますよ
+        .eq('id', id) //idが等しいやつだけ
         .select();
 
     if (error) throw error;
